c943b7353704a1d9f68dcc80bb6c688ed8d99a3d
D29I5\Intern 4 <ralmeida@insticc.org>
2019-01-03 16:01:48 +0000

    .gitignore is now working

--- ignored/credentials.js
+++
@@ -1,8 +0,0 @@
-var credentials = {
-    slackUserName: "slack username",
-    slackPassword: "slack Password",
-    WTMUserName: "WTM username",
-    WTMPassword: "WTM Password",
-}
-
-module.exports = credentials;
